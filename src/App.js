f759648a573cad387d5d04c6ef82c600b7bb0bd9
KeflemT <keflemtn@gmail.com>
2021-11-05 16:11:07 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState} from 'react';
 import './App.css';
+import api from './services/api'
 
-function App() {
+export default function App() {
+
+
+  const [cep, setCep] = useState(0)
+  const [dadosCep, setDadosCep] = useState([])
+  const feedbackEndereco = 'Não existe';
+
+  async function buscarCep() {
+    const dados = await api.get(`${cep}/json`).then((response) => response.data)
+    setDadosCep(dados)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <input
+          type="number"
+          placeholder="digite o cep apenas com numeros"
+          className="input"
+          value={cep}
+          onChange={(e) => setCep(e.target.value)}
+        />
+        <button className="btnBuscaCep" onClick={buscarCep}>
+          Buscar Cep
+        </button>
+        <div>
+          <h1>Bairro: {dadosCep.bairro ? dadosCep.bairro : feedbackEndereco}</h1>
+          <h1>Cep: {dadosCep.cep ? dadosCep.cep : feedbackEndereco}</h1>
+          <h1>Complemento: {dadosCep.complemento ? dadosCep.complemento : feedbackEndereco}</h1>
+          <h1>DDD: {dadosCep.ddd ? dadosCep.ddd : feedbackEndereco}</h1>
+          <h1>localidade: {dadosCep.localidade ? dadosCep.localidade : feedbackEndereco}</h1>
+          <h1>logradouro: {dadosCep.logradouro ? dadosCep.logradouro : feedbackEndereco}</h1>
+        </div>
+      </div>
+  )
 }
-
-export default App;
